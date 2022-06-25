0e2ecd01af20d23b477e6b38682fa1eca0b4773e
Mathis De Witte <mathisdw@icloud.com>
2022-06-25 13:55:28 +0200

    MVC-ified

--- src/main/java/com/example/kernlang/CodebaseViewer.java
+++ src/main/java/com/example/kernlang/CodebaseViewer.java
@@ -1,36 +1,31 @@
 package com.example.kernlang;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 
-public class CodebaseViewer extends Pane {
+public class CodebaseViewer extends Pane implements InvalidationListener {
 
 
     private static CursorState cursorState;
 
     public CodebaseViewer() {
         cursorState = new CursorState(this);
+        cursorState.addListener(this);
         this.setPrefSize(200, 200);
-        this.setOnMouseMoved(e -> {
-            if (cursorState.isDraggingEdge()) {
-                this.getChildren().remove(cursorState.importLine);
-                cursorState.importLine.setEndX(e.getSceneX());
-                cursorState.importLine.setEndY(e.getSceneY());
-                this.getChildren().add(cursorState.importLine);
-            }
-
-        });
+        this.setOnMouseMoved(e -> cursorState.updatePosition(e.getSceneX(), e.getSceneY()));
         this.setOnMouseClicked(e -> {
-            cursorState.startX = e.getSceneX();
-            cursorState.startY = e.getSceneY();
             if (cursorState.isDraggingEdge()) {
                 cursorState.setStateFree();
             } else {
+                cursorState.updateClickedPosition(e.getSceneX(), e.getSceneY());
                 new GraphPopUp(cursorState);
             }
 
         });
+    }
+
+    @Override
+    public void invalidated(Observable observable) {
     }
 }
--- src/main/java/com/example/kernlang/CursorState.java
+++ src/main/java/com/example/kernlang/CursorState.java
@@ -1,20 +1,43 @@
 package com.example.kernlang;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
+import java.util.ArrayList;
+
 /**
  * This class contains some data about the state of the cursor in the window of the application.
- * The object of this class can be passed around by reference as a means of mutating this state from different classes.
+ * It acts as the model of the MVC, the CodebaseViewer on the other hand is the viewer and controller.
  */
-public class CursorState {
+public class CursorState implements Observable {
+
+    private final ArrayList<InvalidationListener> listeners;
+
+    @Override
+    public void addListener(InvalidationListener invalidationListener) {
+        listeners.add(invalidationListener);
+    }
+
+    @Override
+    public void removeListener(InvalidationListener invalidationListener) {
+        listeners.remove(invalidationListener);
+    }
+
+    private void fireInvalidationEvent() {
+        for (InvalidationListener l : listeners) {
+            l.invalidated(this);
+        }
+    }
+
     private enum State {
         DRAGGING_EDGE, DRAGGING_NODE, FREE
     }
 
-    double startX, startY;
-    double currentX, currentY;
+    private double clickedX, clickedY;
+    private double currentX, currentY;
 
     Line importLine;
 
@@ -23,43 +46,47 @@ public class CursorState {
     private final CodebaseViewer cbv;
 
     public CursorState(CodebaseViewer cbv) {
+        this.listeners = new ArrayList<>();
         this.cbv = cbv;
     }
 
     public void setStateDraggingEdge() {
         state = State.DRAGGING_EDGE;
         importLine = new Line();
-        importLine.setStartX(startX);
-        importLine.setStartY(startY);
-        importLine.setEndX(startX);
-        importLine.setEndY(startY);
+        importLine.setStartX(clickedX);
+        importLine.setStartY(clickedY);
+        importLine.setEndX(clickedX);
+        importLine.setEndY(clickedY);
         this.cbv.getChildren().add(importLine);
-    }
-
-    public void setStateDraggingNode() {
-        state = State.DRAGGING_NODE;
     }
 
     public void setStateFree() {
         state = State.FREE;
-    }
-
-    public boolean isDraggingNode() {
-        return state == State.DRAGGING_NODE;
+        fireInvalidationEvent();
     }
 
     public boolean isDraggingEdge() {
         return state == State.DRAGGING_EDGE;
     }
 
-    public boolean isFree() {
-        return state == State.FREE;
+    public void updatePosition(double x, double y) {
+        currentX = x;
+        currentY = y;
+        if (isDraggingEdge()) {
+            importLine.setEndX(currentX);
+            importLine.setEndY(currentY);
+        }
+    }
+
+    public void updateClickedPosition(double x, double y) {
+        clickedX = x;
+        clickedY = y;
     }
 
     public void drawCircle() {
         Circle c = new Circle();
-        c.setCenterX(startX);
-        c.setCenterY(startY);
+        c.setCenterX(clickedX);
+        c.setCenterY(clickedY);
         c.setRadius(20);
         c.setFill(Color.GRAY);
         cbv.getChildren().add(c);
