def28c9166971eddc9a27e04b994ab124b847fda
Mathis De Witte <mathisdw@icloud.com>
2023-06-20 13:43:53 +0200

    removed unnecessary code in db folder

--- src/main/java/com/example/kernlang/db/DAOAbstraction/GraphNodeDAO.java
+++ src/main/java/com/example/kernlang/db/DAOAbstraction/GraphNodeDAO.java
@@ -1,9 +1,7 @@
 package com.example.kernlang.db.DAOAbstraction;
 
-import com.example.kernlang.codebase_viewer.graph.GraphEdge;
 import com.example.kernlang.codebase_viewer.graph.GraphNode;
 import com.example.kernlang.db.DataAccessException;
-import com.example.kernlang.db.EdgeData;
 import com.example.kernlang.db.NodeData;
 
 import java.util.ArrayList;
@@ -16,21 +14,6 @@ public interface GraphNodeDAO {
 
     ArrayList<Integer> getIDOfGraphNodes() throws DataAccessException;
 
-    ArrayList<EdgeData> getAllImports() throws DataAccessException;
-
-    ArrayList<EdgeData> getImportsFromGraphNode(int id) throws DataAccessException;
-
-    ArrayList<EdgeData> getExportsFromGraphNode(int id) throws DataAccessException;
-
     void addNewGraphNode(GraphNode graphNode) throws DataAccessException;
 
-    void addNewImport(GraphEdge graphEdge) throws DataAccessException;
-
-    void addNewImport(EdgeData edgeData) throws DataAccessException;
-
-    void updateGraphNode(GraphNode graphNode) throws DataAccessException;
-
-    void deleteImport(EdgeData edgeData) throws DataAccessException;
-
-    // TODO @mathis add what u need
 }
--- src/main/java/com/example/kernlang/db/jdbc/JDBCGraphNodeDAO.java
+++ src/main/java/com/example/kernlang/db/jdbc/JDBCGraphNodeDAO.java
@@ -1,10 +1,8 @@
 package com.example.kernlang.db.jdbc;
 
-import com.example.kernlang.codebase_viewer.graph.GraphEdge;
 import com.example.kernlang.codebase_viewer.graph.GraphNode;
 import com.example.kernlang.db.DAOAbstraction.GraphNodeDAO;
 import com.example.kernlang.db.DataAccessException;
-import com.example.kernlang.db.EdgeData;
 import com.example.kernlang.db.NodeData;
 
 import java.sql.Connection;
@@ -13,12 +11,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class JDBCGraphNodeDAO extends JDBCAbstractDAO implements GraphNodeDAO {
+public class JDBCGraphNodeDAO implements GraphNodeDAO {
 
     private final Connection connection;
 
     public JDBCGraphNodeDAO(Connection connection) {
-        super(connection);
         this.connection = connection;
     }
 
@@ -33,7 +30,7 @@ public class JDBCGraphNodeDAO extends JDBCAbstractDAO implements GraphNodeDAO {
 
     @Override
     public NodeData getGraphNodeByID(int id) throws DataAccessException {
-        try (PreparedStatement ps = prepare("SELECT * FROM graphnode WHERE id = ?")) {
+        try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM graphnode WHERE id = ?")) {
             ps.setInt(1, id);
             try (ResultSet rs = ps.executeQuery()) {
                 rs.next();
@@ -52,7 +49,7 @@ public class JDBCGraphNodeDAO extends JDBCAbstractDAO implements GraphNodeDAO {
 
     @Override
     public ArrayList<Integer> getIDOfGraphNodes() throws DataAccessException {
-        try (PreparedStatement ps = prepare("SELECT id FROM graphnode")) {
+        try (PreparedStatement ps = connection.prepareStatement("SELECT id FROM graphnode")) {
             try (ResultSet rs = ps.executeQuery()) {
                 ArrayList<Integer> out = new ArrayList<>();
                 while (rs.next()) {
@@ -63,53 +60,6 @@ public class JDBCGraphNodeDAO extends JDBCAbstractDAO implements GraphNodeDAO {
         } catch (SQLException ex) {
             throw new DataAccessException("couln't get the ids from the db", ex);
         }
-    }
-
-    @Override
-    public ArrayList<EdgeData> getAllImports() throws DataAccessException {
-        try (PreparedStatement ps = prepare("SELECT * FROM import")) {
-            try (ResultSet rs = ps.executeQuery()) {
-                ArrayList<EdgeData> out = new ArrayList<>();
-                while (rs.next()) {
-                    out.add(new EdgeData(
-                            rs.getInt("startnode"),
-                            rs.getInt("endnode")
-                    ));
-                }
-                return out;
-            }
-        } catch (SQLException ex) {
-            throw new DataAccessException("couldn't get the imports", ex);
-        }
-    }
-
-    @Override
-    public ArrayList<EdgeData> getImportsFromGraphNode(int id) throws DataAccessException {
-        return getImportExportFromGraphNode(id, "startnode");
-    }
-
-    private ArrayList<EdgeData> getImportExportFromGraphNode(int id, String xxxnode) throws DataAccessException {
-        try (PreparedStatement ps = prepare("SELECT * FROM imports WHERE ? = ?")) {
-            ps.setString(1, xxxnode);
-            ps.setInt(2, id);
-            try (ResultSet rs = ps.executeQuery()) {
-                ArrayList<EdgeData> out = new ArrayList<>();
-                while (rs.next()) {
-                    out.add(new EdgeData(
-                            rs.getInt("startnode"),
-                            rs.getInt("endnode")
-                    ));
-                }
-                return out;
-            }
-        } catch (SQLException ex) {
-            throw new DataAccessException("couldn't get the imports of node with id: " + id, ex);
-        }
-    }
-
-    @Override
-    public ArrayList<EdgeData> getExportsFromGraphNode(int id) throws DataAccessException {
-        return getImportExportFromGraphNode(id, "endnode");
     }
 
     @Override
@@ -125,60 +75,6 @@ public class JDBCGraphNodeDAO extends JDBCAbstractDAO implements GraphNodeDAO {
             } catch (SQLException ex) {
                 System.out.println("nooooo");
                 throw new DataAccessException("couldn't add a new node in the db", ex);
-            }
-        }
-    }
-
-    @Override
-    public void addNewImport(GraphEdge graphEdge) throws DataAccessException {
-        /*if (graphEdge != null) {
-            try (PreparedStatement ps = connection.prepareStatement("INSERT INTO import (startnode, endnode) VALUES (?, ?)")) {
-                ps.setInt(1, graphEdge.getStartNode().getDatabaseID());
-                ps.setInt(2, graphEdge.getEndNode().getDatabaseID());
-                ps.execute();
-            } catch (SQLException ex) {
-                throw new DataAccessException("couldn't make new import in db", ex);
-            }
-        }*/
-    }
-
-    @Override
-    public void addNewImport(EdgeData edgeData) throws DataAccessException {
-        if (edgeData != null) {
-            try (PreparedStatement ps = prepare("INSERT INTO import (startnode, endnode) VALUES (?, ?)")) {
-                ps.setInt(1, edgeData.startID());
-                ps.setInt(2, edgeData.endID());
-                ps.execute();
-            } catch (SQLException ex) {
-                throw new DataAccessException("couldn't make new import in db", ex);
-            }
-        }
-    }
-
-    @Override
-    public void updateGraphNode(GraphNode graphNode) throws DataAccessException {
-        if (graphNode != null) {
-            try (PreparedStatement ps = prepare("UPDATE graphnode SET xpos = ?, ypos = ?, name = ? WHERE id = ?")) {
-                ps.setDouble(1, graphNode.getXProperty().get());
-                ps.setDouble(2, graphNode.getYProperty().get());
-                ps.setString(3, graphNode.getName());
-                ps.setInt(4, graphNode.getDatabaseID());
-                ps.execute();
-            } catch (SQLException ex) {
-                throw new DataAccessException("problem with updating values of graphnode with name/id: " + graphNode.getName() + graphNode.getDatabaseID(), ex);
-            }
-        }
-    }
-
-    @Override
-    public void deleteImport(EdgeData edgeData) throws DataAccessException {
-        if (edgeData != null) {
-            try (PreparedStatement ps = prepare("DELETE FROM import WHERE startnode = ? AND endnode = ?")) {
-                ps.setInt(1, edgeData.startID());
-                ps.setInt(2, edgeData.endID());
-                ps.execute();
-            } catch (SQLException ex) {
-                throw new DataAccessException("couldn't remove import", ex);
             }
         }
     }
